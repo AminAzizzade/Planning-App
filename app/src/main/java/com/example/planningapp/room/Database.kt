888b77692ee33d082ca13e45cf729cc6b58bfa9d
Amin Azizzade <amin.azizcomp24@gmail.com>
2025-02-11 15:55:06 +0300

    Database version 2: Auto-Migration applied

--- app/src/main/java/com/example/planningapp/room/Database.kt
+++ app/src/main/java/com/example/planningapp/room/Database.kt
@@ -1,10 +1,22 @@
 package com.example.planningapp.room
 
+import androidx.room.AutoMigration
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.example.planningapp.data.entity.CheckBoxMission
+import com.example.planningapp.data.entity.Converters
+import com.example.planningapp.data.entity.TaskContent
 import com.example.planningapp.data.entity.TimeLineTask
 
-@Database(entities = [TimeLineTask::class], version = 1)
+@Database(entities = [TimeLineTask::class, TaskContent::class, CheckBoxMission::class],
+    version = 2,
+    autoMigrations = [
+        AutoMigration(from = 1, to = 2)
+    ]
+)
+
+@TypeConverters(Converters::class)
 abstract class Database: RoomDatabase()
 {
     abstract fun getTimeLineTaskDAO(): TimeLineTaskDAO
