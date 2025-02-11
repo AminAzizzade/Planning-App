d2c8effd9b074d6e474643ba59a9e00f587e2692
Amin Azizzade <amin.azizcomp24@gmail.com>
2025-02-11 20:59:18 +0300

    Database Version 2: Creating Datasources for new entities

--- app/src/main/java/com/example/planningapp/data/datasource/CheckBoxMissionDataSource.kt
+++ app/src/main/java/com/example/planningapp/data/datasource/CheckBoxMissionDataSource.kt
@@ -1,4 +1,36 @@
 package com.example.planningapp.data.datasource
 
-class CheckBoxMissionDataSource {
+import com.example.planningapp.data.entity.CheckBoxMission
+import com.example.planningapp.data.entity.TaskContent
+import com.example.planningapp.data.entity.TaskLabel
+import com.example.planningapp.room.CheckBoxMissionDAO
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class CheckBoxMissionDataSource(private val dao: CheckBoxMissionDAO)
+{
+    suspend fun testInsert() = withContext(Dispatchers.IO)
+    {
+        val foreignKey = 1
+        val mission = CheckBoxMission(
+            missionId = 0,
+            contentId = foreignKey,
+            check = true,
+            missionName = "test"
+        )
+
+        dao.insertContent(mission)
+    }
+    suspend fun testRemove() = withContext(Dispatchers.IO)
+    {
+        val foreignKey = 1
+        val mission = CheckBoxMission(
+            missionId = 0,
+            contentId = foreignKey,
+            check = true,
+            missionName = "test"
+        )
+
+        dao.removeContent(foreignKey)
+    }
 }
--- app/src/main/java/com/example/planningapp/data/datasource/ContentOfTaskDataSource.kt
+++ app/src/main/java/com/example/planningapp/data/datasource/ContentOfTaskDataSource.kt
@@ -1,4 +1,36 @@
 package com.example.planningapp.data.datasource
 
-class ContentOfTaskDataSource {
+import com.example.planningapp.data.entity.TaskContent
+import com.example.planningapp.data.entity.TaskLabel
+import com.example.planningapp.room.TaskContentDAO
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class ContentOfTaskDataSource(private val dao: TaskContentDAO)
+{
+    suspend fun testInsert() = withContext(Dispatchers.IO)
+    {
+        val foreignKey = 14379
+        val content = TaskContent(
+            contentId = 0,
+            taskId = foreignKey,
+            missionNote = "test",
+            label = TaskLabel.HOME
+        )
+
+        dao.insertContent(content)
+    }
+
+    suspend fun testRemove() = withContext(Dispatchers.IO)
+    {
+        val foreignKey = 14379
+        val content = TaskContent(
+            contentId = 0,
+            taskId = foreignKey,
+            missionNote = "test",
+            label = TaskLabel.HOME
+        )
+
+        dao.removeContent(foreignKey)
+    }
 }
