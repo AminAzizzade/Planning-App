6a15c366ed9f8f31f53cd81b581251bafe9eb209
Amin Azizzade <amin.azizcomp24@gmail.com>
2025-02-14 20:38:47 +0300

    Database version 2: neseccary updates on DAOs

--- app/src/main/java/com/example/planningapp/room/CheckBoxMissionDAO.kt
+++ app/src/main/java/com/example/planningapp/room/CheckBoxMissionDAO.kt
@@ -7,9 +7,23 @@ import com.example.planningapp.data.entity.CheckBoxMission
 
 @Dao
 interface CheckBoxMissionDAO {
-    @Insert
-    suspend fun insertContent(mission: CheckBoxMission)
 
-    @Query("DELETE FROM CheckBoxMission WHERE contentId = :foreignKey")
-    suspend fun removeContent(foreignKey: Int)
+    @Query("SELECT * FROM CheckBoxMission WHERE ContentID = :contentId")
+    suspend fun getMissions(contentId: Int): List<CheckBoxMission>
+
+    @Insert
+    suspend fun insertMissions(missions: List<CheckBoxMission>)
+
+    @Query("""UPDATE CheckBoxMission SET MissionName = :missionName, "Check" = :check WHERE MissionID = :missionId""")
+    suspend fun updateMission(missionId: Int, missionName: String, check: Boolean)
+
+    @Query("DELETE FROM CheckBoxMission WHERE MissionID = :missionId")
+    suspend fun removeMissions(missionId: Int)
+
+    @Insert
+    suspend fun insertMission(mission: CheckBoxMission)
+
+    @Query("DELETE FROM CheckBoxMission WHERE MissionID = :missionId")
+    suspend fun deleteMission(missionId: Int)
+
 }
--- app/src/main/java/com/example/planningapp/room/TaskContentDAO.kt
+++ app/src/main/java/com/example/planningapp/room/TaskContentDAO.kt
@@ -3,15 +3,22 @@ package com.example.planningapp.room
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 import com.example.planningapp.data.entity.TaskContent
 
 @Dao
 interface TaskContentDAO {
 
+    @Query("SELECT * FROM TaskContent WHERE TaskID = :taskId")
+    suspend fun getContents(taskId: Int): TaskContent
+
     @Insert
     suspend fun insertContent(content: TaskContent)
 
-    @Query("DELETE FROM TaskContent WHERE taskId = :foreignKey")
-    suspend fun removeContent(foreignKey: Int)
+    @Query("UPDATE TaskContent SET Note = :missionNote, Label = :label WHERE ContentID = :contentId")
+    suspend fun updateContent(contentId: Int, label: String, missionNote: String)
+
+    @Query("DELETE FROM TaskContent WHERE ContentID = :contentId")
+    suspend fun removeContent(contentId: Int)
 
 }
