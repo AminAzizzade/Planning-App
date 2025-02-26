fca7248162e224e88ffa8126ef831ce091de3cc1
Amin Azizzade <amin.azizcomp24@gmail.com>
2025-02-26 17:09:05 +0300

    Database Version 3: Necessary changes on AppModule for project feature

--- app/src/main/java/com/example/planningapp/dependencyInjection/AppModule.kt
+++ app/src/main/java/com/example/planningapp/dependencyInjection/AppModule.kt
@@ -6,13 +6,23 @@ import androidx.room.Room
 import com.example.planningapp.data.datasource.CheckBoxMissionDataSource
 import com.example.planningapp.data.datasource.ContentOfTaskDataSource
 import com.example.planningapp.data.datasource.DailyPlanningDataSource
+import com.example.planningapp.data.datasource.project.ProjectDataSource
+import com.example.planningapp.data.datasource.project.ProjectHistoryDataSource
+import com.example.planningapp.data.datasource.project.ProjectTaskDataSource
 import com.example.planningapp.data.repository.CheckBoxMissionRepository
 import com.example.planningapp.data.repository.ContentOfTaskRepository
 import com.example.planningapp.data.repository.DailyPlanningRepository
+import com.example.planningapp.data.repository.project.ProjectHistoryRepository
+import com.example.planningapp.data.repository.project.ProjectRepository
+import com.example.planningapp.data.repository.project.ProjectTaskRepository
 import com.example.planningapp.room.CheckBoxMissionDAO
 import com.example.planningapp.room.Database
 import com.example.planningapp.room.TaskContentDAO
 import com.example.planningapp.room.TimeLineTaskDAO
+import com.example.planningapp.room.project.ProjectDAO
+import com.example.planningapp.room.project.ProjectHistoryDAO
+import com.example.planningapp.room.project.ProjectTaskDAO
+import com.example.planningapp.room.project.ProjectTaskResourceDAO
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -38,6 +48,7 @@ class AppModule
 
 
 
+
     @Provides
     @Singleton
     fun provideContentOfTaskRepository(dataSource: ContentOfTaskDataSource): ContentOfTaskRepository{
@@ -52,6 +63,7 @@ class AppModule
 
 
 
+
     @Provides
     @Singleton
     fun provideCheckBoxMissionRepository(dataSource: CheckBoxMissionDataSource): CheckBoxMissionRepository {
@@ -62,6 +74,51 @@ class AppModule
     @Singleton
     fun provideCheckBoxMissionDataSource(checkBoxMissionDAO: CheckBoxMissionDAO): CheckBoxMissionDataSource {
         return CheckBoxMissionDataSource(checkBoxMissionDAO)
+    }
+
+
+
+
+    @Provides
+    @Singleton
+    fun provideProjectRepository(dataSource: ProjectDataSource): ProjectRepository {
+        return ProjectRepository(dataSource)
+    }
+
+    @Provides
+    @Singleton
+    fun provideProjectDataSource(projectDAO: ProjectDAO): ProjectDataSource {
+        return ProjectDataSource(projectDAO)
+    }
+
+
+
+
+    @Provides
+    @Singleton
+    fun provideProjectHistoryRepository(dataSource: ProjectHistoryDataSource): ProjectHistoryRepository {
+        return ProjectHistoryRepository(dataSource)
+    }
+
+    @Provides
+    @Singleton
+    fun provideProjectHistoryDataSource(projectHistoryDAO: ProjectHistoryDAO): ProjectHistoryDataSource {
+        return ProjectHistoryDataSource(projectHistoryDAO)
+    }
+
+
+
+
+    @Provides
+    @Singleton
+    fun provideProjectTaskRepository(dataSource: ProjectTaskDataSource): ProjectTaskRepository {
+        return ProjectTaskRepository(dataSource)
+    }
+
+    @Provides
+    @Singleton
+    fun provideProjectTaskDataSource(projectTaskDAO: ProjectTaskDAO): ProjectTaskDataSource {
+        return ProjectTaskDataSource(projectTaskDAO)
     }
 
 
@@ -92,6 +149,45 @@ class AppModule
         val database = Room.databaseBuilder(context, Database::class.java, "planyourcareer.db")
             .createFromAsset("planyourcareer.db").build()
         return database.getCheckBoxMissionDAO()
+    }
+
+
+
+
+    @Provides
+    @Singleton
+    fun provideProjectDAO(@ApplicationContext context: Context): ProjectDAO
+    {
+        val database = Room.databaseBuilder(context, Database::class.java, "planyourcareer.db")
+            .createFromAsset("planyourcareer.db").build()
+        return database.getProjectDAO()
+    }
+
+    @Provides
+    @Singleton
+    fun provideProjectHistoryDAO(@ApplicationContext context: Context): ProjectHistoryDAO
+    {
+        val database = Room.databaseBuilder(context, Database::class.java, "planyourcareer.db")
+            .createFromAsset("planyourcareer.db").build()
+        return database.getProjectHistoryDAO()
+    }
+
+    @Provides
+    @Singleton
+    fun provideProjectTaskDAO(@ApplicationContext context: Context): ProjectTaskDAO
+    {
+        val database = Room.databaseBuilder(context, Database::class.java, "planyourcareer.db")
+            .createFromAsset("planyourcareer.db").build()
+        return database.getProjectTaskDAO()
+    }
+
+    @Provides
+    @Singleton
+    fun provideProjectTaskResourceDAO(@ApplicationContext context: Context): ProjectTaskResourceDAO
+    {
+        val database = Room.databaseBuilder(context, Database::class.java, "planyourcareer.db")
+            .createFromAsset("planyourcareer.db").build()
+        return database.getProjectTaskResourceDAO()
     }
 }
 
